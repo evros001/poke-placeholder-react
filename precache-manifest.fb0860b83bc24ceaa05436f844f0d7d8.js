4b6b7e25b30bf63e66136dfb98c8e263af8cd298
Evan Rosse <evanrosse@gmail.com>
2020-09-28 19:40:43 -0400

    Updates

---
+++ precache-manifest.fb0860b83bc24ceaa05436f844f0d7d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa5e127f0837592fc113f3936e8e3eaa",
+    "url": "/poke-placeholder-react/index.html"
+  },
+  {
+    "revision": "94c0601959f52f2c2622",
+    "url": "/poke-placeholder-react/static/css/main.2e338c7d.chunk.css"
+  },
+  {
+    "revision": "303bab7bada25981a00e",
+    "url": "/poke-placeholder-react/static/js/2.21ffab2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/poke-placeholder-react/static/js/2.21ffab2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94c0601959f52f2c2622",
+    "url": "/poke-placeholder-react/static/js/main.7bc2cd81.chunk.js"
+  },
+  {
+    "revision": "d2cb4c2ef528a0d918e5",
+    "url": "/poke-placeholder-react/static/js/runtime-main.bfc058ca.js"
+  }
+]);
